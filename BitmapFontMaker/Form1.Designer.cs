bdc321c3f03fc2ac004020965cb5a9730619d5e7
captain <980097191@qq.com>
2017-09-06 21:24:15 +0800

    Upload

---
+++ BitmapFontMaker/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace BitmapFontMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 503);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "MakeFont";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(101, 68);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(495, 280);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "128",
+            "256",
+            "512",
+            "1024",
+            "2048"});
+            this.checkedListBox1.Location = new System.Drawing.Point(238, 368);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 84);
+            this.checkedListBox1.TabIndex = 2;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "字体图片尺寸";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 616);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BitmapFontMaker/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BitmapFontMaker
+{
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AllowDrop = true;
+
+
+        }
+
+        //private void Form1_DragEnter(object sender,DragEventArgs e)
+        //{
+        //    if(e.Data.GetDataPresent(DataFormats.FileDrop))
+        //    {
+        //        string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+        //        foreach (string f in files)
+        //        {
+        //            listBox1.Items.Add(f);
+        //        }
+        //    }
+        //}
+
+        public class Vector2
+        {
+            public int X;
+            public int Y;
+
+            public Vector2(int varX,int varY)
+            {
+                X = varX;
+                Y = varY;
+            }
+        }
+
+
+        public void Pack(string[] varMiniPngPath)
+        {
+            string tmpDirPath = varMiniPngPath[0].Substring(0, varMiniPngPath[0].LastIndexOf(Path.DirectorySeparatorChar));
+
+            string tmpPackedSavePath = tmpDirPath + "/packed.png";
+            if(File.Exists(tmpPackedSavePath))
+            {
+                File.Delete(tmpPackedSavePath);
+            }
+
+            string tmpPackedTextSavePath= tmpDirPath + "/packed.fnt";
+            if (File.Exists(tmpPackedTextSavePath))
+            {
+                File.Delete(tmpPackedTextSavePath);
+            }
+            StreamWriter tmpStreamWrite = new StreamWriter(tmpPackedTextSavePath);
+
+            string tmpDescribeHeader = "info face=\"Arial\" size=32 bold=0 italic=0 charset=\"\" unicode=1 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=1,1 outline=0 common lineHeight = 32 base = 26 scaleW = {0} scaleH = {1} pages = 1 packed = 0 alphaChnl = 1 redChnl = 0 greenChnl = 0 blueChnl = 0";
+            tmpStreamWrite.WriteLine(string.Format(tmpDescribeHeader, mPackedSize, mPackedSize));
+
+            Bitmap tmpBitMapPacked = new Bitmap(mPackedSize, mPackedSize);
+
+            bool[,] tmpBitMapPackedPixelFillStatus = new bool[tmpBitMapPacked.Width,tmpBitMapPacked.Height];
+
+            for (int tmpBitMapPacked_X = 0; tmpBitMapPacked_X < tmpBitMapPacked.Width; tmpBitMapPacked_X++)
+            {
+                for (int tmpBitMapPacked_y = 0; tmpBitMapPacked_y < tmpBitMapPacked.Height; tmpBitMapPacked_y++)
+                {
+                    tmpBitMapPackedPixelFillStatus[tmpBitMapPacked_X,tmpBitMapPacked_y] = false;
+                }
+            }
+
+            string tmpDescribe = "char id={0}   x={1}     y={2}    width={3}    height={4}    xoffset={5}     yoffset={6}     xadvance={7}    page=0  chnl=15";
+
+
+            for (int tmpMiniPngPathIndex = 0; tmpMiniPngPathIndex < varMiniPngPath.Length; tmpMiniPngPathIndex++)
+            {
+                string tmpMiniPngPath = varMiniPngPath[tmpMiniPngPathIndex];
+
+                string tmpMiniPnfFileName = Path.GetFileNameWithoutExtension(tmpMiniPngPath);
+                char[] tmpAsciiArray = tmpMiniPnfFileName.ToCharArray();
+                if(tmpAsciiArray.Length!=1)
+                {
+                    MessageBox.Show("小图片命名不规范，命名为单字符,比如 A B C 1 2 3 # * 之类");
+
+                    tmpStreamWrite.Close();
+                    return;
+                }
+                int tmpAscii = (int)tmpAsciiArray[0];
+                
+
+                Image tmpImageMini = Image.FromFile(tmpMiniPngPath);
+
+                Bitmap tmpBitMapMini = new Bitmap(tmpImageMini);
+
+
+                //找一块空白的区域
+                Vector2 tmpVector2Start = null;
+                bool tmpFindEmptyArea = false;
+                for (int tmpBitMapPacked_X = 0; tmpBitMapPacked_X < tmpBitMapPacked.Width; tmpBitMapPacked_X++)
+                {
+                    for (int tmpBitMapPacked_y = 0; tmpBitMapPacked_y < tmpBitMapPacked.Height; tmpBitMapPacked_y++)
+                    {
+                        if (tmpBitMapPackedPixelFillStatus[tmpBitMapPacked_X, tmpBitMapPacked_y] == false)
+                        {
+                            //X轴 遍历 从左到右 找到 一个 没有被覆盖的像素点
+                            tmpVector2Start = new Vector2(tmpBitMapPacked_X, tmpBitMapPacked_y);
+
+                            //首先判断剩余空间是否足够放下小图
+                            if (tmpVector2Start.X + tmpBitMapMini.Width > tmpBitMapPacked.Width || tmpVector2Start.Y + tmpBitMapMini.Height > tmpBitMapPacked.Height)
+                            {
+                                continue;
+                            }
+
+                            //从起始点开始，加上 小图的宽 高，获得一个区域，遍历这个区域，是否是空白区域
+                            bool tmpAreaEmpty = true;
+                            for (int tmpCheckFillIndex_X = tmpVector2Start.X; tmpCheckFillIndex_X < tmpVector2Start.X+tmpBitMapMini.Width; tmpCheckFillIndex_X++)
+                            {
+                                for (int tmpCheckFillIndex_Y = tmpVector2Start.Y; tmpCheckFillIndex_Y < tmpVector2Start.Y+tmpBitMapMini.Height; tmpCheckFillIndex_Y++)
+                                {
+                                    if (tmpBitMapPackedPixelFillStatus[tmpCheckFillIndex_X, tmpCheckFillIndex_Y])
+                                    {
+                                        tmpAreaEmpty = false;
+                                        break;
+                                    }
+                                }
+                                if (tmpAreaEmpty == false)
+                                {
+                                    break;
+                                }
+                            }
+
+                            tmpFindEmptyArea = tmpAreaEmpty;
+                            if (tmpFindEmptyArea)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (tmpFindEmptyArea)
+                    {
+                        break;
+                    }
+                }
+
+                if (tmpFindEmptyArea == false)
+                {
+                    break;
+                }
+
+                for (int tmpX = 0; tmpX < tmpBitMapMini.Width; tmpX++)
+                {
+                    for (int tmpY = 0; tmpY < tmpBitMapMini.Height; tmpY++)
+                    {
+                        tmpBitMapPackedPixelFillStatus[tmpX + tmpVector2Start.X, tmpY + tmpVector2Start.Y] = true;
+
+                        tmpBitMapPacked.SetPixel(tmpX + tmpVector2Start.X, tmpY + tmpVector2Start.Y, tmpBitMapMini.GetPixel(tmpX, tmpY));
+
+                        //tmpBitMapPacked.SetPixel(tmpX + tmpVector2Start.X, tmpY + tmpVector2Start.Y, Color.Red);
+                    }
+                }
+
+                //string tmpDescribe = "char id={0}   x={1}     y={2}    width={3}    height={4}    xoffset={5}     yoffset={6}     xadvance={7}    page=0  chnl=15";
+
+                //string tmpDescribeFinal=  string.Format(tmpDescribe, tmpAscii, tmpVector2Start.X,tmpBitMapPacked.Height -( tmpVector2Start.Y+tmpBitMapMini.Height), tmpImageMini.Width, tmpImageMini.Height, 0, 0, tmpImageMini.Width);
+                string tmpDescribeFinal = string.Format(tmpDescribe, tmpAscii, tmpVector2Start.X, tmpVector2Start.Y , tmpImageMini.Width, tmpImageMini.Height, 0, -tmpImageMini.Height/2, tmpImageMini.Width);
+                tmpStreamWrite.WriteLine(tmpDescribeFinal);
+                tmpStreamWrite.Flush();
+            }
+
+           
+
+            tmpBitMapPacked.Save(tmpPackedSavePath);
+            tmpStreamWrite.Close();
+        }
+
+        private int mPackedSize = 256;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(checkedListBox1.CheckedItems==null || checkedListBox1.CheckedItems.Count!=1)
+            {
+                MessageBox.Show("先勾选上面的字体图片尺寸");
+                return;
+            }
+
+            string tmpPackedSize = checkedListBox1.CheckedItems[0].ToString();
+            mPackedSize = int.Parse(tmpPackedSize);
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //打开的对话框可以选择多个文件
+            ofd.Multiselect = true;
+
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+
+                for (int i = 0; i < ofd.FileNames.Length; i++)
+                {
+                    listBox1.Items.Add(ofd.FileNames[i]);
+                }
+
+                Pack(ofd.FileNames);
+
+                MessageBox.Show("Finish");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
